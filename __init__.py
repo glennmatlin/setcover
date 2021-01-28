2785e718806a2c40a1ee583622626d6065a77c10
glennmatlin <glenn.matlin@gmail.com>
2021-01-28 15:45:37 -0800

    init

--- __init__.py
+++ __init__.py
@@ -1,18 +1,9 @@
 __author__ = "Glenn Matlin"
 
-import sys
-
 # This relies on each of the submodules having an __all__ variable.
 from .src.cover import *
 from .src.set import *
 
 __all__ = (cover.__all__ + set.__all__)
-
-if sys.platform == 'win32':  # pragma: no cover
-    from .windows_events import *
-    __all__ += windows_events.__all__
-else:
-    from .unix_events import *  # pragma: no cover
-    __all__ += unix_events.__all__
 
 __version__ = '0.0.1'
