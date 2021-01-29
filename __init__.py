d8fbe5415de480ebde4d0acb1667f884a626a533
glennmatlin <glenn.matlin@gmail.com>
2021-01-28 16:04:25 -0800

    init

--- __init__.py
+++ __init__.py
@@ -1,9 +1,2 @@
 __author__ = "Glenn Matlin"
-
-# This relies on each of the submodules having an __all__ variable.
-from .src.cover import *
-from .src.set import *
-
-__all__ = (cover.__all__ + set.__all__)
-
 __version__ = '0.0.1'
