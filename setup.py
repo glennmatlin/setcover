f3fbd8c168d5800f4494f7a4bd8bc791ed73bc59
glennmatlin <glenn.matlin@gmail.com>
2021-02-05 13:22:54 -0800

    fix typo

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="SetCoverage",
+    name="pyspark-setcover",
     version="0.2.0",
     packages=find_packages(),
-    description="Set Coverage",
+    description="pyspark-setcover",
     author="Glenn Matlin",
     author_email="glenn.matlin@gmail.com",
 )
