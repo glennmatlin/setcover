146039297910565d7ea391bb971d8e4d2a7c232f
glennmatlin <glenn.matlin@gmail.com>
2021-02-08 16:03:00 -0800

    minor version bump

--- setcover/__init__.py
+++ setcover/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.2.1"
+__version__ = "0.3.0"
 
 from .set import WeightedSet, ExclusionSet
 from .weighted import WeightedSetCoverProblem
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pyspark-setcover",
-    version="0.2.1",
+    version="0.3.0",
     packages=find_packages(),
     description="pyspark-setcover",
     author="Glenn Matlin",
